22f2b5c733631773862aa2d8a32242254a5329ed
Vanilton Filho <vanilton.alv3sfilho@gmail.com>
2023-07-27 22:25:56 -0300

    feat: add sum function

---
+++ math_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+
+	total := Soma(15, 15)
+
+	if total != 30 {
+		t.Errorf("Resultado da soma é inválido: \nResultado %d. \nEsperado: %d", total, 30)
+	}
+}
